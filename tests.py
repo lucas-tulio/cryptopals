bc976fb92a1559118874c44d32493b17c7e23b00
Lucas <lucasdnd@gmail.com>
2015-11-15 16:12:14 -0200

    Detect single-character XOR

--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
 from utils import hex_to_base64
 from utils import fixed_xor
 from utils import single_byte_xor
+from utils import find_message
 
-# 1. Hex to Base64
+# 1-1. Hex to Base64
 
 hex_input = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 b64_expected_result = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
@@ -11,7 +12,7 @@ b64_result = hex_to_base64(hex_input)
 assert b64_expected_result == b64_result
 print('1-1. Hex to Base64: passed')
 
-# 2. Fixed XOR
+# 1-2. Fixed XOR
 
 xor_input = 0x1c0111001f010100061a024b53535009181c
 xor_key = 0x686974207468652062756c6c277320657965
@@ -21,11 +22,21 @@ xor_result = fixed_xor(xor_input, xor_key)
 assert xor_expected_result == xor_result
 print('1-2. Fixed XOR: passed')
 
-# 3. Single byte XOR
+# 1-3. Single byte XOR
+
 hex_str = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 sbx_expected_result = 'Cooking MC\'s like a pound of bacon'
+
 sbx_result = single_byte_xor(hex_str)
 assert sbx_expected_result == sbx_result
 print('1-3. Single-byte XOR: passed')
+
+# 1-4. Detect single-character XOR
+
+input_file = './files/sc-xor-input-file.txt'
+message = find_message(input_file)
+expected_message = 'Now that the party is jumping\n'
+assert message == expected_message
+print('1-4. Detect single-character XOR: passed')
 
 print('All tests passed')
--- utils.py
+++ utils.py
@@ -1,70 +1,14 @@
 import string
 
 base64_table = {
-  0 : "A",
-  1 : "B",
-  2 : "C",
-  3 : "D",
-  4 : "E",
-  5 : "F",
-  6 : "G",
-  7 : "H",
-  8 : "I",
-  9 : "J",
-  10: "K",
-  11: "L",
-  12: "M",
-  13: "N",
-  14: "O",
-  15: "P",
-  16: "Q",
-  17: "R",
-  18: "S",
-  19: "T",
-  20: "U",
-  21: "V",
-  22: "W",
-  23: "X",
-  24: "Y",
-  25: "Z",
-  26: "a",
-  27: "b",
-  28: "c",
-  29: "d",
-  30: "e",
-  31: "f",
-  32: "g",
-  33: "h",
-  34: "i",
-  35: "j",
-  36: "k",
-  37: "l",
-  38: "m",
-  39: "n",
-  40: "o",
-  41: "p",
-  42: "q",
-  43: "r",
-  44: "s",
-  45: "t",
-  46: "u",
-  47: "v",
-  48: "w",
-  49: "x",
-  50: "y",
-  51: "z",
-  52: "0",
-  53: "1",
-  54: "2",
-  55: "3",
-  56: "4",
-  57: "5",
-  58: "6",
-  59: "7",
-  60: "8",
-  61: "9",
-  62: "+",
-  63: "/",
+  0 : "A", 1 : "B", 2 : "C", 3 : "D", 4 : "E", 5 : "F", 6 : "G", 7 : "H",
+  8 : "I", 9 : "J", 10: "K", 11: "L", 12: "M", 13: "N", 14: "O", 15: "P",
+  16: "Q", 17: "R", 18: "S", 19: "T", 20: "U", 21: "V", 22: "W", 23: "X",
+  24: "Y", 25: "Z", 26: "a", 27: "b", 28: "c", 29: "d", 30: "e", 31: "f",
+  32: "g", 33: "h", 34: "i", 35: "j", 36: "k", 37: "l", 38: "m", 39: "n",
+  40: "o", 41: "p", 42: "q", 43: "r", 44: "s", 45: "t", 46: "u", 47: "v",
+  48: "w", 49: "x", 50: "y", 51: "z", 52: "0", 53: "1", 54: "2", 55: "3",
+  56: "4", 57: "5", 58: "6", 59: "7", 60: "8", 61: "9", 62: "+", 63: "/",
 }
 
 def hex_to_base64(input_string):
@@ -114,6 +58,14 @@ def fixed_xor(hex_input, hex_key):
   # XOR
   return hex_input ^ hex_key
 
+def calc_score(sentence):
+  # Score the resulting sentence according to the number of uppercase/lowercase/space characters
+  score = 0
+  for character in sentence:
+    if character in string.ascii_lowercase or character in string.ascii_uppercase or character == ' ':
+      score += 1
+  return score
+
 def single_byte_xor(hex_str):
 
   sentences = {}
@@ -124,19 +76,41 @@ def single_byte_xor(hex_str):
     # XOR each number against i
     result = ""
     for j in range(0, len(hex_str), 2):
+      num = hex_str[j:j+2]
+      if num == '\n':
+        continue
       num = '0x' + hex_str[j:j+2]
       new_num = int(num, 16) ^ i
       result += chr(new_num)
 
-    # Score the resulting sentence according to the number of uppercase/lowercase/space characters
-    score = 0
-    for character in result:
-      if character in string.ascii_lowercase or character in string.ascii_uppercase or character == ' ':
-        score += 1
+    score = calc_score(result)
 
     sentences[result] = score
 
   # Result: the sentence with the max score
   message = max(sentences.keys(), key=(lambda k: sentences[k]))
   return message
+
+def find_message(input_file):
+  """
+  Find the string encoded by a single-character XOR in the input_file
+  """
+  with open(input_file) as f:
+    
+    max_score = 0
+    result = ""
+    
+    for line in f:
+      sentence = single_byte_xor(line)
+      score = calc_score(sentence)
+      if score >= max_score:
+        max_score = score
+        result = sentence
+
+  return result
+
+
+
+
+
 
